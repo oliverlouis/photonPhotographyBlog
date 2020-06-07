9f43dcb1e9f68847ff62caf614a8a11ad4f1298e
Oliver Louis <oliverlouis@me.com>
2020-06-07 15:55:04 +0200

    added Blog, Comment aund User Models. Added authentication with passport

--- app.js
+++ app.js
@@ -1,16 +1,41 @@
 const express = require('express');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
-const colors = require('colors');
-const methodOverride = require('method-override');
-const expressSanitizer = require('express-sanitizer');
-const app = express();
+(bodyParser = require('body-parser')),
+	(mongoose = require('mongoose')),
+	(colors = require('colors')),
+	(Comment = require('./models/comment')),
+	(Blog = require('./models/blog')),
+	(User = require('./models/user')),
+	(methodOverride = require('method-override')),
+	(passport = require('passport')),
+	(LocalStrategy = require('passport-local')),
+	(expressSanitizer = require('express-sanitizer')),
+	(app = express());
 
 //CONNECT MONGOOSE TO MONGO DB
 mongoose
 	.connect('mongodb://localhost/photon', {useNewUrlParser: true, useUnifiedTopology: true})
 	.then(() => console.log('Connected to Database!'.yellow.bold))
 	.catch((err) => console.log("Couldn't connect to database", err));
+
+//PASSPORT CONFIG
+app.use(
+	require('express-session')({
+		secret: 'I love Arielle so much',
+		resave: false,
+		saveUninitialized: false,
+	})
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+app.use((req, res, next) => {
+	res.locals.currentUser = req.user;
+	next();
+});
 
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
@@ -19,14 +44,6 @@ app.use(expressSanitizer());
 app.use(methodOverride('_method'));
 
 //MONGOOSE/MODEL CONFIG
-const blogSchema = new mongoose.Schema({
-	title: String,
-	image: String,
-	body: String,
-	created: {type: Date, default: Date.now},
-});
-
-const Blog = mongoose.model('Blog', blogSchema);
 
 //RESTFUL ROUTES
 //INDEX
@@ -43,10 +60,9 @@ app.get('/blogs', (req, res) => {
 		}
 	});
 });
-
-app.listen(3000, () => {
-	console.log('Photon Jar Server started'.blue.bold);
-});
+//===================================================================
+//CREATE BLOG ROUTES
+//===================================================================
 
 //NEW ROUTE
 app.get('/blogs/new', (req, res) => {
@@ -106,4 +122,47 @@ app.delete('/blogs/:id', (req, res) => {
 			res.redirect('/blogs');
 		}
 	});
+});
+
+//===================================================================
+//AUTHENTICATION AND SIGN UP ROUTES
+//===================================================================
+
+//SIGN UP
+//show sign up form
+app.get('/register', (req, res) => {
+	res.render('register');
+});
+
+//handle sign up logic
+app.post('/register', (req, res) => {
+	User.register(new User({username: req.body.username}), req.body.password, (err, user) => {
+		if (err) {
+			console.log(err);
+			return res.render('register');
+		} else {
+			passport.authenticate('local')(req, res, () => {
+				res.redirect('/blogs');
+			});
+		}
+	});
+});
+
+//LOG IN
+//show log in form
+app.get('/login', (req, res) => {
+	res.render('login');
+});
+
+//handle login logic
+app.post('/login', passport.authenticate('local', {successRedirect: '/blogs', failureRedirect: '/login'}), (req, res) => {});
+
+//handle logout logic
+app.get('/logout', (req, res) => {
+	req.logout();
+	res.redirect('/blogs');
+});
+
+app.listen(3000, () => {
+	console.log('Photon Server started'.blue.bold);
 });
---
+++ models/blog.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+const blogSchema = new mongoose.Schema({
+	title: String,
+	image: String,
+	body: String,
+	author: {
+		id: {
+			type: mongoose.Schema.Types.ObjectId,
+			ref: 'User',
+		},
+		username: String,
+	},
+	created: {type: Date, default: Date.now},
+	comments: [
+		{
+			type: mongoose.Schema.Types.ObjectId,
+			ref: 'Comment',
+		},
+	],
+});
+
+module.exports = mongoose.model('Blog', blogSchema);
